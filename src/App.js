48ae33e3b8879ccd185059872504392aa2c44c3c
freedev3394 <haseebiqbal3394@gmail.com>
2022-05-18 19:45:38 +0500

    upload file

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App() {
+  const [filename,setFileName] = useState("")
+  const [message,setMessage] = useState("")
+  const uploadFile = (e) => {
+    setFileName(e.target.files[0].name)
+    setMessage("You upload a file! Thanks")
+  }
+  
+  return (
+    <div className="App">
+      <h1>Upload a file and find the name of file</h1>
+          <input type="file" onChange={(e)=>uploadFile(e)}/>
+          <h1>File name is : {filename}</h1>
+          <h1>{message}</h1>
+          
+    </div>
+  );
+}
+
+export default App;
